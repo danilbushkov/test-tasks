2ccf425bdd1c052edcbe6bfd90c4b6b059df6bc2
danilbushkov <danil1357997@gmail.com>
2024-08-04 22:26:34 +0300

    add pagination

--- import-products/app/Http/Controllers/Pages/ProductController.php
+++ import-products/app/Http/Controllers/Pages/ProductController.php
@@ -13,17 +13,37 @@ class ProductController extends Controller
     public function list(Request $request): RedirectResponse | View {
         $page = $request->input('page');
         if($page !== null) {
-            if(!is_int($page)) {
-                return redirect('/products');
+            if(!ctype_digit($page) || (int)$page < 1) {
+                return redirect('/products?page=1');
             }
 
         } else {
             $page = 1;
         }
+        $maxItems = 10;
+        $count = Product::count();
+        $maxPage = ceil($count / $maxItems);
+        $products = [];
+        if($maxPage == 0 ) {
+            if($page > 1) {
+                return redirect('/products?page=1');
+            }
+        } else {
+            if($page > $maxPage) {
+                return redirect('/products?page='.$maxPage);
+            }
+            $products = Product::select('id', 'name')
+                ->offset(($page-1)*$maxItems)
+                ->limit($maxItems)->get();
+        }
 
-        $products = Product::select('name')->get();
 
-        return view('products.list', ['products' => $products]);
+
+        return view('products.list', [
+            'products' => $products,
+            'page' => $page,
+            'maxPage' => $maxPage
+        ]);
     }
 
     public function import(): string {
--- import-products/resources/views/products/list.blade.php
+++ import-products/resources/views/products/list.blade.php
@@ -5,6 +5,21 @@
 @section('content')
     <p>Список товаров</p>
     @foreach ($products as $product)
-        <a href="/products/1">{{ $product->name }}</a><br/>
+        <a href="/products/1">{{ $product->id }} {{ $product->name }}</a><br/>
     @endforeach
+    @if ($page > 1)
+        <a href="/products?page={{$page-1}}">{{"<"}}</a>
+    @endif
+    @if ($maxPage != 0 && $page != 1)
+        <a href="/products?page=1">{{ 1 }}</a>
+    @endif
+    @if ($maxPage != 0)
+        <span>{{ $page }}</span>
+    @endif
+    @if ($maxPage != 0 && $page != $maxPage)
+        <a href="/products?page={{$maxPage}}">{{ $maxPage }}</a>
+    @endif
+    @if ($page < $maxPage)
+        <a href="/products?page={{$page+1}}">{{">"}}</a>
+    @endif
 @endsection
