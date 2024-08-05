6e55956f86b7e602fb5da14b9c3bcf1752d25eb7
danilbushkov <danil1357997@gmail.com>
2024-08-05 09:43:26 +0300

    skip duplicate lines

--- import-products/app/Imports/ProductsImport.php
+++ import-products/app/Imports/ProductsImport.php
@@ -6,9 +6,7 @@ use App\Models\AdditionalProductField;
 use App\Models\Product;
 use App\Models\ProductPicture;
 use Illuminate\Support\Collection;
-use Maatwebsite\Excel\Concerns\PersistRelations;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 
@@ -18,41 +16,44 @@ HeadingRowFormatter::default('none');
 
 class ProductsImport implements ToCollection, WithHeadingRow
 {
-
     public function collection(Collection $rows): Void
     {
         foreach ($rows as $row)
         {
-            $product = Product::create([
-                'external_code' => $row['Внешний код'],
-                'name' => $row['Наименование'] ?? "",
-                'description' => $row['Описание'] ?? "",
-                'price' => str_replace(',', '.', $row['Цена: Цена продажи'] ?? 0),
-                'discount' => str_replace(',', '.', $row['Скидка'] ?? 0)
-            ]);
+            $externalCode = $row['Внешний код'];
+            if(!Product::where('external_code', $externalCode)->exists()) {
+                $product = Product::create([
+                    'external_code' => $externalCode,
+                    'name' => $row['Наименование'] ?? "",
+                    'description' => $row['Описание'] ?? "",
+                    'price' => str_replace(',', '.', $row['Цена: Цена продажи'] ?? 0),
+                    'discount' => str_replace(',', '.', $row['Скидка'] ?? 0)
+                ]);
 
-            foreach($row as $key => $value) {
-                if(str_starts_with($key, 'Доп. поле:') && $value !== null) {
-                    $key = str_replace("Доп. поле: ", "", $key);
-                    if($key === 'Ссылки на фото') {
-                        $links = explode(',', str_replace(' ', '', $value));
-                        foreach($links as $link) {
-                            ProductPicture::create([
-                                'path' => "",
-                                'link' => $link,
+                foreach($row as $key => $value) {
+                    if(str_starts_with($key, 'Доп. поле:') && $value !== null) {
+                        $key = str_replace("Доп. поле: ", "", $key);
+                        if($key === 'Ссылки на фото') {
+                            $links = explode(',', str_replace(' ', '', $value));
+                            foreach($links as $link) {
+                                ProductPicture::create([
+                                    'path' => "",
+                                    'link' => $link,
+                                    'product_id' => $product->id
+                                ]);
+                            }
+                        } else {
+                            AdditionalProductField::create([
+                                'key' => $key,
+                                'value' => $value,
                                 'product_id' => $product->id
                             ]);
                         }
-                    } else {
-                        AdditionalProductField::create([
-                            'key' => $key,
-                            'value' => $value,
-                            'product_id' => $product->id
-                        ]);
                     }
                 }
             }
         }
     }
+
 }
 
