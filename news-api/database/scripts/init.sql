95e89936f26ee7696c852823cfacad7f91d512eb
danilbushkov <danil1357997@gmail.com>
2024-08-08 17:40:42 +0300

    create database

--- news-api/database/scripts/init.sql
+++ news-api/database/scripts/init.sql
@@ -2,45 +2,18 @@
 -- Структура таблицы `News`
 --
 
-CREATE TABLE `News` (
-  `Id` bigint NOT NULL,
-  `Title` tinytext NOT NULL,
-  `Content` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE news (
+  id BIGSERIAL PRIMARY KEY,
+  title VARCHAR(256) NOT NULL,
+  content TEXT NOT NULL
+);
 
--- --------------------------------------------------------
+CREATE TABLE news_categories (
+  news_id BIGINT NOT NULL,
+  category_id BIGINT NOT NULL,
+  PRIMARY KEY(news_id, category_id),
+  FOREIGN KEY(news_id) 
+    REFERENCES news(id)
+    ON DELETE CASCADE
+);
 
---
--- Структура таблицы `NewsCategories`
---
-
-CREATE TABLE `NewsCategories` (
-  `NewsId` bigint NOT NULL,
-  `CategoryId` bigint NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `News`
---
-ALTER TABLE `News`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Индексы таблицы `NewsCategories`
---
-ALTER TABLE `NewsCategories`
-  ADD PRIMARY KEY (`NewsId`,`CategoryId`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `News`
---
-ALTER TABLE `News`
-  MODIFY `Id` bigint NOT NULL AUTO_INCREMENT;
