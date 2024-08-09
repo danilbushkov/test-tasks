0bc9781fa002fdbc8cf154353b74599a6738cfe0
danilbushkov <danil1357997@gmail.com>
2024-08-09 14:38:33 +0300

    add constructor for NewsHandlers

--- news-api/internal/app/api/handlers/news/news.go
+++ news-api/internal/app/api/handlers/news/news.go
@@ -5,3 +5,9 @@ import "github.com/danilbushkov/test-tasks/internal/app/context"
 type NewsHandlers struct {
 	appContext *context.AppContext
 }
+
+func New(actx *context.AppContext) *NewsHandlers {
+	return &NewsHandlers{
+		appContext: actx,
+	}
+}
