9019239815432f2fe6e71987916dedf90cce915e
danilbushkov <danil1357997@gmail.com>
2024-08-10 19:57:09 +0300

    add categories insert

--- news-api/internal/app/storages/news/add.go
+++ news-api/internal/app/storages/news/add.go
@@ -6,6 +6,7 @@ import (
 	"github.com/danilbushkov/test-tasks/internal/app/db/models"
 	"github.com/danilbushkov/test-tasks/internal/app/errors"
 	"github.com/danilbushkov/test-tasks/internal/app/structs"
+	"gopkg.in/reform.v1"
 )
 
 func (s *NewsStorage) Add(news *structs.News) (int64, error) {
@@ -19,7 +20,34 @@ func (s *NewsStorage) Add(news *structs.News) (int64, error) {
 		Title:   *news.Title,
 		Content: *news.Content,
 	}
-	if err := s.db.Orm().Save(n); err != nil {
+	tx, err := s.db.Orm().Begin()
+	if err != nil {
+		return 0, fmt.Errorf("%w: %w", errors.ErrDatabase, err)
+	}
+
+	defer tx.Rollback()
+
+	if err = tx.Save(n); err != nil {
+		return 0, fmt.Errorf("%w: %w", errors.ErrDatabase, err)
+	}
+
+	if news.Categories != nil && len(*news.Categories) != 0 {
+		categories := []reform.Struct{}
+		for i := 0; i < len(*news.Categories); i++ {
+			category := models.NewsCategories{
+				NewID:      n.ID,
+				CategoryId: (*news.Categories)[i],
+			}
+			categories = append(categories, &category)
+		}
+		err = tx.InsertMulti(categories...)
+		if err != nil {
+			return 0, fmt.Errorf("%w: %w", errors.ErrDatabase, err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
 		return 0, fmt.Errorf("%w: %w", errors.ErrDatabase, err)
 	}
 
