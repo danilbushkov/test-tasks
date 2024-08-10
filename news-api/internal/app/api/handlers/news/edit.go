63a736de847e8a39a0297a4f924b241086557889
danilbushkov <danil1357997@gmail.com>
2024-08-10 18:36:38 +0300

    add news editing

--- news-api/internal/app/api/handlers/news/edit.go
+++ news-api/internal/app/api/handlers/news/edit.go
@@ -1,7 +1,38 @@
 package news
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"errors"
+	"strconv"
+
+	app_err "github.com/danilbushkov/test-tasks/internal/app/errors"
+	ns "github.com/danilbushkov/test-tasks/internal/app/storages/news"
+	"github.com/danilbushkov/test-tasks/internal/app/structs"
+	"github.com/gofiber/fiber/v2"
+)
 
 func (nh *NewsHandlers) Edit(c *fiber.Ctx) error {
-	return c.SendString("Edit")
+	p := c.Params("id")
+	id, err := strconv.ParseInt(p, 10, 64)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	n := new(structs.News)
+
+	if err := c.BodyParser(n); err != nil {
+		nh.appContext.Log.Info(err)
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	n.Id = &id
+	newsStorage := ns.NewFromAppContext(nh.appContext)
+	if err := newsStorage.Edit(n); err != nil {
+		if errors.Is(err, app_err.ErrDatabase) {
+			nh.appContext.Log.Error(err)
+			return c.SendStatus(fiber.StatusInternalServerError)
+		} else {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(structs.Error{
+				Message: err.Error(),
+			})
+		}
+	}
+	return c.SendStatus(fiber.StatusNoContent)
 }
---
+++ news-api/internal/app/storages/news/edit.go
@@ -0,0 +1,43 @@
+package news
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/danilbushkov/test-tasks/internal/app/db/models"
+	e "github.com/danilbushkov/test-tasks/internal/app/errors"
+	"github.com/danilbushkov/test-tasks/internal/app/structs"
+	"gopkg.in/reform.v1"
+)
+
+func (s *NewsStorage) Edit(news *structs.News) error {
+	columns := []string{}
+	if err := news.CheckId(); err != nil {
+		return err
+	}
+	n := &models.News{ID: *news.Id}
+	if news.Title != nil {
+		if err := news.CheckTitle(); err != nil {
+			return err
+		}
+		n.Title = *news.Title
+		columns = append(columns, "title")
+	}
+	if news.Content != nil {
+		if err := news.CheckContent(); err != nil {
+			return err
+		}
+		n.Content = *news.Content
+		columns = append(columns, "content")
+	}
+
+	if err := s.db.Orm().UpdateColumns(n, columns...); err != nil {
+		if err == reform.ErrNoRows {
+			return errors.New("News did not exist")
+		} else {
+			return fmt.Errorf("%w: %w", e.ErrDatabase, err)
+		}
+	}
+
+	return nil
+}
