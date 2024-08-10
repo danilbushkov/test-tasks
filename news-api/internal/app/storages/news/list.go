199bfe7647733962d7ba634d63faf57d67e3524c
danilbushkov <danil1357997@gmail.com>
2024-08-10 20:30:45 +0300

    select categories

--- news-api/internal/app/storages/news/list.go
+++ news-api/internal/app/storages/news/list.go
@@ -22,11 +22,30 @@ func (s *NewsStorage) List() ([]*structs.News, error) {
 		if err = s.db.Orm().NextRow(&news, rows); err != nil {
 			break
 		}
+		categories := []int64{}
+		r, err := s.db.Orm().FindRows(
+			models.NewsCategoriesView,
+			"news_id", news.ID)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %w", errors.ErrDatabase, err)
+		}
+		defer r.Close()
+
+		for {
+			var category models.NewsCategories
+			if err = s.db.Orm().NextRow(&category, r); err != nil {
+				break
+			}
+			categories = append(categories, category.CategoryId)
+		}
+		if err != reform.ErrNoRows {
+			return nil, fmt.Errorf("%w: %w", errors.ErrDatabase, err)
+		}
 		list = append(list, &structs.News{
 			Id:         &news.ID,
 			Title:      &news.Title,
 			Content:    &news.Content,
-			Categories: &[]int64{},
+			Categories: &categories,
 		})
 	}
 	if err != reform.ErrNoRows {
